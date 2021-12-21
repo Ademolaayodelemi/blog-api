7e6b5e0da8824ea8d4c4f77562511c029d92b00c
Ademolaayodelemi <ademolaayodelemi@gmail.com>
2021-12-21 22:24:56 +0100

    Update tag.blade.php

--- resources/views/front/tag.blade.php
+++ resources/views/front/tag.blade.php
@@ -11,7 +11,7 @@
             <img src="{{ asset('imgs/'.$post->featured_img) }}" class="card-img-top mb-3" alt="{{ $post->post_title }}">
             </a>
             <p>{{ str_limit($post->post_body,100,'...') }}</p>
-            <p><a href="{{ url('post/'.$post->post_slug.'/'.$post->post_id) }}" class="btn btn-dark btn-sm">Read More...</a></p>
+            <p><a href="{{ url('post/'.$post->post_slug.'/'.$post->post_id) }}" class="btn btn-dark btn-sm">Read More....</a></p>
         </div>
         <div class="card-footer">
             <small><b>Last Updated: </b> {{ $post->updated_at }}</small>
